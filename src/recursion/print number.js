0adea15854a6ec0586cf3d139d94b695a85ad510
Muhsin <muhsintt777@gmail.com>
2026-08-31 19:23:39 +0530

    add function to print numbers in reverse order

--- src/recursion/print number.js
+++ src/recursion/print number.js
@@ -6,3 +6,12 @@ function printNumber(n, curr = 0) {
 }
 
 printNumber(5);
+
+function printNumberReverse(n, curr = n) {
+  if (curr < 1) return;
+
+  console.log(curr);
+  printNumberReverse(n, curr - 1);
+}
+
+printNumberReverse(5);
