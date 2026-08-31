f16eedffdcfe3e77db9fb10eef600711d4df6df6
Muhsin <muhsintt777@gmail.com>
2026-08-31 15:10:31 +0530

    add function to check if a number is a power of two

---
+++ src/leetcode/231. Power of Two.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function (n) {
+  if (n <= 0) return false;
+  console.log(n & (n - 1));
+  return (n & (n - 1)) === 0;
+};
+
+// the '&' operator compare bit wise 'AND'
